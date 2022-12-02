dae9d1c46b55c7d5d0161686a1048317bb6fc7b5
Jimbravo <jimbravo05@hotmail.com>
2022-12-01 21:36:10 -0500

    fix

--- app/src/main/java/pe/edu/ulima/pm/demoextrasapp/ui/modules/library/BookDetail.kt
+++ app/src/main/java/pe/edu/ulima/pm/demoextrasapp/ui/modules/library/BookDetail.kt
@@ -5,6 +5,8 @@ import androidx.compose.foundation.background
 import androidx.compose.foundation.border
 import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.*
+import androidx.compose.material.Button
+import androidx.compose.material.OutlinedButton
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
@@ -13,6 +15,7 @@ import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import androidx.navigation.NavHostController
+import java.time.LocalDateTime
 import kotlin.math.round
 
 @Composable
@@ -28,8 +31,10 @@ fun BookDetail(navigatorController: NavHostController) {
     )
     {
         Column(
-            Modifier.size(100.dp)
-                .border(width = 2.dp, Color.Red),
+            Modifier
+                .width(260.dp)
+                .height(150.dp)
+                .border(width = 2.dp, Color.Black),
         ){
             Text(text = "Titulo: ", fontSize = 30.sp)
             Text(text = "Autor: ")
@@ -38,14 +43,38 @@ fun BookDetail(navigatorController: NavHostController) {
             Text(text = "ISBN: ")
         }
         Column(
-            Modifier.size(100.dp)
+            Modifier
+                .width(260.dp)
+                .height(50.dp)
                 .border(width = 2.dp, Color.Black),
         ){
-            Text(text = "Titulo: ", fontSize = 30.sp)
-            Text(text = "Autor: ")
-            Text(text = "Pie de imprenta: ")
-            Text(text = "Descripcion fisica: ")
-            Text(text = "ISBN: ")
+            Text(text = "Codigo de clasificacion: ")
+            Text(text = "Localización: ")
+            Text(text = "Copias: ")
+        }
+        Spacer(modifier = Modifier.width(8.dp))
+        Row(
+            Modifier
+                .width(260.dp)
+                .height(50.dp)
+                .border(width = 2.dp, Color.Black),
+        ) {
+            Text(text = "Disponibilidad: ")
+
+            Button(
+                onClick = { },
+            ) {
+                Text("RESERVAR")
+            }
+        }
+
+        Spacer(modifier = Modifier.width(8.dp))
+
+        Button(
+            onClick = { },
+            modifier = Modifier.align(Alignment.CenterHorizontally)
+        ) {
+            Text("NOTIFICAR DE DISPONIBILIDAD")
         }
     }
 }
